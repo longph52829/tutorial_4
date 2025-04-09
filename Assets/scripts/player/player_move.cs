aa80a0e851a53cc6916e193b9e83a9f3527bbc7d
longph52829 <longdhph52829@gmail.com>
2025-04-09 13:55:36 +0700

    up4

--- Assets/scripts/player/player_move.cs
+++ Assets/scripts/player/player_move.cs
@@ -5,7 +5,7 @@ public class player_move : MonoBehaviour
     Rigidbody2D rb;
     Animator ani;
     float movement;
-    bool isGrounded, doubleJump;
+    public bool isGrounded, doubleJump;
 
     [SerializeField] float moveSpeed, jumpForce;
     [SerializeField] Transform groundCheck;
@@ -21,7 +21,28 @@ public class player_move : MonoBehaviour
     void Update()
     {
         CheckGrounded();
-        //checkJump();
+        checkJump();
+
+        if (Input.GetKeyDown(KeyCode.Space) || (Input.GetKeyDown(KeyCode.UpArrow)))
+        {
+            if (isGrounded)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+                doubleJump = true;
+                ani.SetBool("is_jump", true);
+            }
+            else if (doubleJump)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+                doubleJump = false;
+                ani.SetBool("is_double_jump", true);
+            }
+        }
+        else if (isGrounded && rb.linearVelocity.y <= 0)
+        {
+            ani.SetBool("is_jump", false);
+            ani.SetBool("is_double_jump", false);
+        }
     }
 
     void FixedUpdate()
@@ -37,7 +58,7 @@ public class player_move : MonoBehaviour
             transform.localScale = new Vector3(-1, 1, 1);
         }
 
-        checkAnimationFloat("move", Mathf.Abs(movement));
+        ani.SetFloat("move", Mathf.Abs(movement));
     }
 
     void OnMove(InputValue value)
@@ -45,43 +66,49 @@ public class player_move : MonoBehaviour
         movement = value.Get<float>();
     }
 
-    void OnJump(InputValue value)
-    {
-        if (value.isPressed)
-        {
-            if (isGrounded)
-            {
-                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-                doubleJump = true;
-                //checkAnimationBool("is_jump", true);
-            }
-            else if (doubleJump)
-            {
-                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-                doubleJump = false;
-                //checkAnimationBool("is_double_jump", true);
-            }
-        }
-    }
+    // void OnJump(InputValue value)
+    // {
+    //     // Debug.Log("OnJump called");
+    //     // Debug.Log($"isGrounded: {isGrounded}, Velocity Y: {rb.linearVelocity.y}");
+        
+    //     if (value.isPressed)
+    //     {
+    //         Debug.Log(value.isPressed);
+    //         Debug.Log("Jump pressed");
+    //         if (isGrounded)
+    //         {
+    //             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    //             doubleJump = true;
+    //             ani.SetBool("is_jump", true);
+    //         }
+    //         else if (doubleJump)
+    //         {
+    //             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    //             doubleJump = false;
+    //             ani.SetBool("is_double_jump", true);
+    //         }
+    //     }
+    //     else if(!value.isPressed)
+    //     {
+           
+    //         // Debug.Log("Jump released");
+    //         // Debug.Log($"isGrounded: {isGrounded}, Velocity Y: {rb.linearVelocity.y}");
+    //         if (isGrounded && rb.linearVelocity.y <= 0)
+    //         {
+    //             ani.SetBool("is_jump", false);
+    //             ani.SetBool("is_double_jump", false);
+    //         }
+    //     }
+    // }
 
     void CheckGrounded()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
 
-    // void checkJump()
-    // {
-    //     checkAnimationFloat("jump", rb.linearVelocity.y);
-    //     checkAnimationFloat("double_jump", rb.linearVelocity.y);
-    // }
-
-    void checkAnimationBool(string name, bool condition)
+    void checkJump()
     {
-        ani.SetBool(name, condition);
-    }
-
-    void checkAnimationFloat(string name, float value)
-    {
-        ani.SetFloat(name, value);
+        ani.SetFloat("jump", rb.linearVelocity.y);
+        ani.SetFloat("double_jump", rb.linearVelocity.y);
     }
 }
